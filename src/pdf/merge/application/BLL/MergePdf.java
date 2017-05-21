30567b515cded039420089be3f2792f5d552a4e8
basox70 <basile.dubruque@epsi.fr>
2017-05-21 22:50:21 +0200

    Adding destination folder and name for merged pdf

--- src/pdf/merge/application/BLL/MergePdf.java
+++ src/pdf/merge/application/BLL/MergePdf.java
@@ -1,43 +1,23 @@
 package pdf.merge.application.BLL;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.pdf.*;
 
 import java.io.*;
-import java.net.URL;
-import java.util.*;
 
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.apache.pdfbox.tools.PDFMerger;
 
 public class MergePdf {
 
-    public static void Merge(File[] fileList, String Destination) throws IOException{
+	@SuppressWarnings("deprecation")
+	public static void Merge(File[] fileList, String Destination) throws IOException{
         PDFMergerUtility ut = new PDFMergerUtility();
         
-//        int totalPages = 0;
-//
-//        // Create writer for the outputStream
-//        PdfWriter writer = PdfWriter.getInstance(document, outputStream);
-//
-//        //Open document.
-//        document.open();
-//
-//        //Contain the pdf data.
-//        PdfContentByte pageContentByte = writer.getDirectContent();
-//
-//        PdfImportedPage pdfImportedPage;
-//        int currentPdfReaderPage = 1;
-//        java.util.Iterator<PdfReader> iteratorPDFReader = readers.iterator();
-
-        // Iterate and process the reader list.
+        // adding files from fileList
         for (int i = 0; i < fileList.length; i++) {
-        	
         	ut.addSource(fileList[i].getPath());
-        	
         }
+        
         ut.setDestinationFileName(Destination);
-    	ut.mergeDocuments();;
+    	ut.mergeDocuments();
 
         System.out.println("Les fichiers ont bien étés fusionnés.");
     }
--- src/pdf/merge/application/DTO/MainWindow.java
+++ src/pdf/merge/application/DTO/MainWindow.java
@@ -8,222 +8,260 @@ package pdf.merge.application.DTO;
 
 import pdf.merge.application.BLL.*;
 
-import javax.swing.JFrame;
-import javax.swing.JFileChooser;
+import javax.swing.*;
 
+import java.awt.GridLayout;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 
 public class MainWindow extends javax.swing.JFrame {
-    
+
 	private static final long serialVersionUID = 1L;
 	private String windowTitle = "Fusionneur de PDF";
-    private String chooserDialogTitle = "Sélectionnez un dossier";
-    private String noFolderSelectedMessage = "Aucun dossier n'a été sélectionné";
-    
-    private File[] fileList;
-    private String absolutePath;
-    
-    private javax.swing.JMenu Aide;
-    private javax.swing.JMenu Editer;
-    private javax.swing.JMenuItem Exit;
-    private javax.swing.JMenuItem MergePDF;
-    private javax.swing.JMenuItem AddText;
-    private javax.swing.JMenuItem OpenFolder;
-    private javax.swing.JMenu File;
-    private javax.swing.JMenuItem Settings;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea listContainer;
-    
-    /**
-     * Creates new form mainWindow
-     */
-    public MainWindow() {
-        initComponents();
-        this.setTitle(windowTitle);
-        this.setSize(750, 500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        Editer.setEnabled(false);
-        this.setVisible(true);
-    }
+	private static String chooserDialogTitle = "Sélectionnez un dossier";
+	private String noFolderSelectedMessage = "Aucun dossier n'a été sélectionné";
 
-    private void initComponents() {
+	private static File[] fileList;
+	private String absolutePath;
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        listContainer = new javax.swing.JTextArea();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        File = new javax.swing.JMenu();
-        OpenFolder = new javax.swing.JMenuItem();
-        Settings = new javax.swing.JMenuItem();
-        Exit = new javax.swing.JMenuItem();
-        Editer = new javax.swing.JMenu();
-        MergePDF = new javax.swing.JMenuItem();
-        AddText = new javax.swing.JMenuItem();
-        Aide = new javax.swing.JMenu();
+	private JMenu Aide;
+	private JMenu Editer;
+	private JMenuItem Exit;
+	private JMenuItem MergePDF;
+	private JMenuItem AddText;
+	private JMenuItem OpenFolder;
+	private JMenu File;
+	private JMenuItem Settings;
+	private JMenuBar jMenuBar1;
+	private JScrollPane jScrollPane1;
+	private JTextArea listContainer;
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setResizable(false);
+	/**
+	 * Creates new form mainWindow
+	 */
+	public MainWindow() {
+		initComponents();
+		this.setTitle(windowTitle);
+		this.setSize(750, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		Editer.setEnabled(false);
+		this.setVisible(true);
+	}
 
-        listContainer.setEditable(false);
-        listContainer.setColumns(20);
-        listContainer.setRows(5);
-        listContainer.setText("Sélectionnez un dossier");
-        listContainer.setFocusable(false);
-        listContainer.setMargin(new java.awt.Insets(10, 10, 10, 10));
-        jScrollPane1.setViewportView(listContainer);
+	// Popup to select destination file and folder
+	private void display() {
+		String path = fileList[0].getPath();
+		JButton button = new JButton("Choisir");
+		JTextField field1 = new JTextField("Mon PDF");
+		JTextField field2 = new JTextField("C:\\");
+		JPanel panel = new JPanel(new GridLayout(0, 2));
+		panel.add(new JLabel("Nom du fichier (.pdf):"));
+		panel.add(field1);
+		panel.add(new JLabel("Dossier de destination :"));
+		panel.add(button);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				String tmp = field2.getText();
+				chooser.setCurrentDirectory(new java.io.File(field2.getText()));
+				chooser.setDialogTitle(chooserDialogTitle);
+				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				chooser.setAcceptAllFileFilterUsed(false);
 
-        File.setText("Fichier");
-
-        OpenFolder.setText("Ouvrir un dossier");
-        OpenFolder.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                OpenFolderActionPerformed(evt);
-            }
-        });
-        File.add(OpenFolder);
-        OpenFolder.getAccessibleContext().setAccessibleDescription("");
-
-        Settings.setText("Réglages");
-
-        File.add(Settings);
-
-        Exit.setText("Quitter");
-        Exit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                ExitActionPerformed(evt);
-            }
-        });
-        File.add(Exit);
-
-        jMenuBar1.add(File);
-
-        Editer.setText(" Editer");
-
-        MergePDF.setText("Fusionner les fichiers");
-        MergePDF.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-            	MergePDFActionPerformed(evt);
-            }
-        });
-        
-        AddText.setText("Ajouter du texte");
-        AddText.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                AddTextActionPerformed(evt);
-            }
-        });
-
-        Editer.add(MergePDF);
-        
-        Editer.add(AddText);
-
-        jMenuBar1.add(Editer);
-
-        Aide.setText("Aide");
-
-        jMenuBar1.add(Aide);
-
-        setJMenuBar(jMenuBar1);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-
-        pack();
-    }
-    protected void MergePDFActionPerformed(ActionEvent evt) {
-    	try {
-			MergePdf.Merge(fileList, "D:\\Documents\\Ecole\\EPSI\\Cours B3-2\\JAVA\\projet java\\merged\\test.pdf");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+					field2.setText(chooser.getSelectedFile().getAbsolutePath());
+				} else {
+					field2.setText(tmp);
+				}
+			}
+		});
+		field2.setColumns(2);
+		panel.add(field2);
+		int result = JOptionPane.showConfirmDialog(null, panel, "Dossier de fusion", JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.PLAIN_MESSAGE);
+		if (result == JOptionPane.OK_OPTION) {
+			System.out.println(field1.getText() + "\\" + field2.getText());
+			String destFile = field2.getText() + "\\"+ field1.getText() + ".pdf";
+			System.out.println(destFile);
+			try {
+				MergePdf.Merge(fileList, destFile);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		} else {
+			System.out.println("Cancelled");
 		}
+	}
+
+	private void initComponents() {
+
+		jScrollPane1 = new javax.swing.JScrollPane();
+		listContainer = new javax.swing.JTextArea();
+		jMenuBar1 = new javax.swing.JMenuBar();
+		File = new javax.swing.JMenu();
+		OpenFolder = new javax.swing.JMenuItem();
+		Settings = new javax.swing.JMenuItem();
+		Exit = new javax.swing.JMenuItem();
+		Editer = new javax.swing.JMenu();
+		MergePDF = new javax.swing.JMenuItem();
+		AddText = new javax.swing.JMenuItem();
+		Aide = new javax.swing.JMenu();
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+
+		listContainer.setEditable(false);
+		listContainer.setColumns(20);
+		listContainer.setRows(5);
+		listContainer.setText("Sélectionnez un dossier");
+		listContainer.setFocusable(false);
+		listContainer.setMargin(new java.awt.Insets(10, 10, 10, 10));
+		jScrollPane1.setViewportView(listContainer);
+
+		File.setText("Fichier");
+
+		OpenFolder.setText("Ouvrir un dossier");
+		OpenFolder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				OpenFolderActionPerformed(evt);
+			}
+		});
+		File.add(OpenFolder);
+		OpenFolder.getAccessibleContext().setAccessibleDescription("");
+
+		Settings.setText("Réglages");
+
+		File.add(Settings);
+
+		Exit.setText("Quitter");
+		Exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				ExitActionPerformed(evt);
+			}
+		});
+		File.add(Exit);
+
+		jMenuBar1.add(File);
+
+		Editer.setText(" Editer");
+
+		MergePDF.setText("Fusionner les fichiers");
+		MergePDF.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				display();
+			}
+		});
+
+		AddText.setText("Ajouter du texte");
+		AddText.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				AddTextActionPerformed(evt);
+			}
+		});
+
+		Editer.add(MergePDF);
+
+		Editer.add(AddText);
+
+		jMenuBar1.add(Editer);
+
+		Aide.setText("Aide");
+
+		jMenuBar1.add(Aide);
+
+		setJMenuBar(jMenuBar1);
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup().addContainerGap()
+						.addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+						.addContainerGap()));
+		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(
+				javax.swing.GroupLayout.Alignment.TRAILING,
+				layout.createSequentialGroup().addContainerGap()
+						.addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
+						.addContainerGap()));
+
+		pack();
 	}
 
 	private void AddTextActionPerformed(ActionEvent evt) {
 		PDFEdit.Edit(fileList);
 	}
-    
-    private void OpenFolderActionPerformed(ActionEvent evt) {//GEN-FIRST:event_OpenFolderActionPerformed
-        displayBrowserWindow();
-    }
 
-    private void ExitActionPerformed(ActionEvent evt) {//GEN-FIRST:event_ExitActionPerformed
-        System.exit(0);
-    }
+	private void OpenFolderActionPerformed(ActionEvent evt) {
+		displayBrowserWindow();
+	}
 
+	private void ExitActionPerformed(ActionEvent evt) {
+		System.exit(0);
+	}
 
-    private void displayBrowserWindow() {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setCurrentDirectory(new java.io.File("Desktop"));
-        chooser.setDialogTitle(chooserDialogTitle);
-        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        chooser.setAcceptAllFileFilterUsed(false);
-        
-        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-            absolutePath = chooser.getSelectedFile().getAbsolutePath();
-            fileList = FileFinder.find(absolutePath);
-            printFileNames(fileList);
-            Editer.setEnabled(true);
-        } else {
-            listContainer.setText("");
-            listContainer.append(noFolderSelectedMessage);
-        }
-    }
-    
-    private void printFileNames(File[] fileList) {
-        String fileNames = "";
-        
-        for (int i = 0; i < fileList.length; i++) {
-            fileNames += fileList[i].getName() + "\n";
-        }
-        
-        listContainer.setText("");
-        listContainer.append(fileNames);
-    }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
+	private void displayBrowserWindow() {
+		JFileChooser chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new java.io.File("Desktop"));
+		chooser.setDialogTitle(chooserDialogTitle);
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.setAcceptAllFileFilterUsed(false);
 
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainWindow().setVisible(true);
-            }
-        });
-    }
+		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			absolutePath = chooser.getSelectedFile().getAbsolutePath();
+			fileList = FileFinder.find(absolutePath);
+			printFileNames(fileList);
+			Editer.setEnabled(true);
+		} else {
+			listContainer.setText("");
+			listContainer.append(noFolderSelectedMessage);
+		}
+	}
+
+	private void printFileNames(File[] fileList) {
+		String fileNames = "";
+
+		for (int i = 0; i < fileList.length; i++) {
+			fileNames += fileList[i].getName() + "\n";
+		}
+
+		listContainer.setText("");
+		listContainer.append(fileNames);
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String args[]) {
+
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null,
+					ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null,
+					ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null,
+					ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null,
+					ex);
+		}
+		// </editor-fold>
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new MainWindow().setVisible(true);
+			}
+		});
+	}
 
 }
